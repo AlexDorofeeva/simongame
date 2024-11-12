3e954096f01b5fa0e7cea2830929c2905a537db0
AlexDorofeeva <124511553+AlexDorofeeva@users.noreply.github.com>
2024-11-12 16:50:38 +0800

    Update game.js

--- game.js
+++ game.js
@@ -10,6 +10,8 @@ $(".btn").click(function() {
   userClickedPattern.push(userChosenColor);
   playSound(userChosenColor);
    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length-1);
+
 });
 
 
@@ -23,6 +25,11 @@ function nextSequence() {
 
   $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 playSound(randomChosenColor);
+
+userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level " + level);
 
 
 
@@ -47,7 +54,10 @@ function animatePress(currentColor) {
   }, 100);
 }
 
-
+  $(document).addClass("game-over");
+  setTimeout(function () {
+    $(document).removeClass("game-over");
+  }, 200);
 
   $(document).keypress(function() {
     if (!started) {
@@ -57,7 +67,40 @@ function animatePress(currentColor) {
     }
   });
 
+  function checkAnswer(currentLevel) {
 
-function checkAnswer(currentLevel) {
-  checkAnswer();
+      //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+      if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+        console.log("success");
+
+        //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+        if (userClickedPattern.length === gamePattern.length){
+
+          //5. Call nextSequence() after a 1000 millisecond delay.
+          setTimeout(function () {
+            nextSequence();
+          }, 1000);
+
+        }
+
+      } else {
+
+        console.log("wrong");
+          playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function () {
+          $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game over, Press any key to restart")
+  startOver();
+      }
+
+  }
+
+
+function startOver() {
+  level = 0;
+ gamePattern = [];
+ started = false;
 }
